ddc862689e29c11b13210ea6bf5215a77d5c0fcf
quinnjo554 <quinnjohnson24@gmail.com>
2023-07-19 12:39:05 -0500

    if no task table wont show

--- src/components/Desktop/TaskList.tsx
+++ src/components/Desktop/TaskList.tsx
@@ -2,8 +2,6 @@ import { useTaskProject } from "@/queries/getQueries";
 import {
   Avatar,
   Box,
-  List,
-  ListItem,
   Table,
   TableCaption,
   Tbody,
@@ -21,45 +19,53 @@ function TaskList(props: { projectId: string }) {
     isLoading: taskLoading,
     isError: userError,
   } = useTaskProject(props.projectId);
+
   if (taskLoading) {
     return <div>Loading</div>;
   }
+
   return (
-    <Box m={2} p={2} height="15rem" overflowY={"scroll"} rounded={"lg"}>
-      <Box rounded={"lg"} p={3} boxShadow={"dark-lg"}>
-        <Table variant="striped">
-          <TableCaption></TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Task Name</Th>
-              <Th>Assigned</Th>
-              <Th>Status</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {tasks?.content.map((value, index) => (
-              <Tr key={index}>
-                <Td>{value.taskName}</Td>
-                <Td fontSize={"sm"}>
-                  <Tooltip
-                    rounded={"md"}
-                    hasArrow
-                    label={value.assignee?.userName}
-                  >
-                    <Avatar
-                      size={"md"}
-                      _hover={{}}
-                      name={value.assignee?.userName ?? ""}
-                      src={value.assignee?.img ?? ""}
-                    ></Avatar>
-                  </Tooltip>
-                </Td>
-                <Td>{value.status}</Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </Box>
+    <Box>
+      {tasks?.content.length ?? 0 > 0 ? (
+        <Box m={2} p={2} height="20rem" overflowY={"scroll"} rounded={"lg"}>
+          <Box rounded={"lg"} p={3} boxShadow={"dark-lg"}>
+            <Table variant="striped">
+              <TableCaption></TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Task Name</Th>
+                  <Th>Assigned</Th>
+                  <Th>Status</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {tasks?.content.map((value, index) => (
+                  <Tr key={index}>
+                    <Td>{value.taskName}</Td>
+                    <Td fontSize={"sm"}>
+                      <Tooltip
+                        rounded={"md"}
+                        hasArrow
+                        label={value.assignee?.userName}
+                      >
+                        <Avatar
+                          size={"md"}
+                          _hover={{}}
+                          name={value.assignee?.userName ?? ""}
+                          src={value.assignee?.img ?? ""}
+                        ></Avatar>
+                      </Tooltip>
+                    </Td>
+                    <Td>{value.status}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Box>
+        </Box>
+      ) : (
+        <></>
+      )}
     </Box>
   );
 }
